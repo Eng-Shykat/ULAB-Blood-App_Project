5e1aeb2470c7249b8756ecf1e01eccb0d4451ea2
Eng-Shykat <sazzadul.islam.cse@ulab.edu.bd>
2023-12-21 00:16:10 +0600

    create notification

--- Submission 4/lib/repository/notification_repository.dart
+++ Submission 4/lib/repository/notification_repository.dart
@@ -4,16 +4,4 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../data/model/notification_data_model.dart';
 import '../provider/global_provider.dart';
 
-final notificationRepositoryProvider =
-    StreamProvider<QuerySnapshot<NotificationDataModel>>((ref) {
-  return ref
-      .read(firestoreProvider)
-      .collection('notifications')
-      .orderBy("createdAt", descending: true)
-      .withConverter<NotificationDataModel>(
-          fromFirestore: (map, options) {
-            return NotificationDataModel.fromMap(map.data()!);
-          },
-          toFirestore: (model, options) => model.toMap())
-      .snapshots();
-});
+
