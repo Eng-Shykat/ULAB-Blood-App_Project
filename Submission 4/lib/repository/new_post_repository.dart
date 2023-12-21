3b3054897beb937b369cf8f71f5a775ae3426216
Eng-Shykat <sazzadul.islam.cse@ulab.edu.bd>
2023-12-21 23:06:01 +0600

    add some module

---
+++ Submission 4/lib/repository/new_post_repository.dart
@@ -0,0 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ulab_blood_bank/data/model/post_data_model.dart';
+
+class NewPostRepository {
+  void createPost(PostDataModel postDataModel) async {
+    final _firestore = FirebaseFirestore.instance;
+
+  }
+}
